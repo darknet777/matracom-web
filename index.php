0cf456ac407ddbbdcfece635e178ebf6346e3b7c
darknet777 <skyfallbladez@gmail.com>
2021-09-07 17:44:26 +0800

    Changes in styles.css and index.php

--- index.php
+++ index.php
@@ -6,22 +6,49 @@
 	<title>Welcome to Matracom Official Site</title>
 	<link rel="shortcut icon" type="image/png" href="assets/android.png">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<div class="container-fluid">
-		<div class="row align-item-center justify-content-start">
+		<div class="row align-item-center justify-content-start bg-dark text-white">
 			<div class="col-auto me-auto">
 				<img src="assets/android.png" class="col-md-1 float-md-start mb-3 ms-md-3" width="50px">
 				<h1>MATRACOM</h1>
 			</div>
-			<div class="col navbar">
-				<div class="nav-item"><a href="#">Beranda</a></div>
-				<div class="nav-item"><a href="#">Profil</a></div>
-				<div class="nav-item"><a href="#">Galeri</a></div>
-				<div class="nav-item"><a href="#">Tentang</a></div>
+			<div class="col navbar nav justify-content-end">
+				<div class="nav-item"><a href="#" class="nav-link text-white">Beranda</a></div>
+				<div class="nav-item"><a href="#" class="nav-link text-white">Profil</a></div>
+				<div class="nav-item"><a href="#" class="nav-link text-white">Galeri</a></div>
+				<div class="nav-item"><a href="#" class="nav-link text-white">Tentang</a></div>
+				<div class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle text-white" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Login</a>
+					<div class="dropdown-menu">
+						<div><a class="dropdown-item" href="#">Masuk</a></div>
+						<div><a class="dropdown-item" href="#">Registrasi</a></div>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div></div>
+		<div>
+				<div class="nav justify-content-center">
+					<div><a class="fa fa-facebook" href="#"></a></div>
+					<div><a class="fa fa-linkedin" href="#"></a></div>
+					<div><a class="text-muted" href="#"></a></div>
+				</div>
+			<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+				<p class="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
+				<a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+					<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+				</a>
+				<ul class="nav justify-content-end">
+					<li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Beranda</a></li>
+					<li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Profil</a></li>
+					<li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Galeri</a></li>
+					<li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Tentang</a></li>
+				</ul>
+			</footer>
+		</div>
 	</div>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
