6778d57fcc8e4275adae5319f7eb6b4e0e277a54
darknet777 <skyfallbladez@gmail.com>
2021-09-07 16:50:21 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome to Matracom Official Site</title>
+	<link rel="shortcut icon" type="image/png" href="assets/android.png">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row align-item-center justify-content-start">
+			<div class="col-auto me-auto">
+				<img src="assets/android.png" class="col-md-1 float-md-start mb-3 ms-md-3" width="50px">
+				<h1>MATRACOM</h1>
+			</div>
+			<div class="col navbar">
+				<div class="nav-item"><a href="#">Beranda</a></div>
+				<div class="nav-item"><a href="#">Profil</a></div>
+				<div class="nav-item"><a href="#">Galeri</a></div>
+				<div class="nav-item"><a href="#">Tentang</a></div>
+			</div>
+		</div>
+		<div></div>
+	</div>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
